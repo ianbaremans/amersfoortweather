b9bf80395e8f5428fe60faa360944be0f8991371
ianbaremans <ianbaremans@gmail.com>
2017-11-04 14:30:57 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from darksky import forecast
+
+# global vars for easy change
+locationlong = "5.387827"
+locationlat = "52.156111" 
+secret_key_file = open("secret_key.txt", "r")
+secret_key = secret_key_file.readline()
+
