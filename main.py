d6d74c7a4cf55c7b2182c112e1b7fea96dc5f3a4
ianbaremans <ianbaremans@gmail.com>
2017-11-11 13:14:18 +0100

    Working requests added

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-from darksky import forecast
+import requests
+import datetime
 
 # global vars for easy change
-locationlong = "5.387827"
-locationlat = "52.156111" 
-secret_key_file = open("secret_key.txt", "r")
-secret_key = secret_key_file.readline()
+locationlong = 5.387827
+locationlat = 52.156111 
+base_url = "https://api.darksky.net/forecast/{}/{},{}?units=si&exclude="
+with open("secret_key.txt") as secret_key_file:
+    secret_key = secret_key_file.read().replace("\n", "").replace("\r", "")
+
+now = datetime.datetime.now()
+today_date = now.strftime("%A %d-%m-%Y") 
+
+response = requests.get(base_url.format(secret_key, locationlat, locationlong))
+forecast = response.json()
+print(today_date)
+print(forecast["daily"]["summary"])
 
