3f8f3429f072a95a766d9a8b4f01756d82e4f20b
ianbaremans <ianbaremans@gmail.com>
2017-11-25 12:38:43 +0100

    Script finished

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import datetime
+import time
 
 # global vars for easy change
 longitude = 5.387827
@@ -8,10 +9,10 @@ request = "https://api.darksky.net/forecast/{}/{},{}?units=si"
 time_machine = "https://api.darksky.net/forecast/{}/{},{},{}?units=si" 
 with open("secret_key.txt") as secret_key_file:
     secret_key = secret_key_file.read().replace("\n", "").replace("\r", "")
+unixtime = int(time.time())
 
 now = datetime.datetime.now()
 today_date = now.strftime("%A %d-%m-%Y") 
-time_date = int(now.strftime("%Y%m%d")) 
 
 response = requests.get(request.format(secret_key, latitude, longitude))
 forecast = response.json()
@@ -20,8 +21,9 @@ print("Today is "+today_date+". The forecast for today is: ")
 print(forecast["currently"]["summary"])
 print(" -"*int(len(forecast["currently"]["summary"])/2))
 
-for num in range(time_date, time_date+7):
+for num in range(unixtime, unixtime+604800, 86400):
     time_response = requests.get(time_machine.format(secret_key, latitude, longitude, num))
     future = time_response.json()
-    print(future["currently"]["summary"])
+    day = datetime.datetime.fromtimestamp(num).strftime("%A")
+    print("{}: {}".format(day, future["currently"]["summary"]))
 
