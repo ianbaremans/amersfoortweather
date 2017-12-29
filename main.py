ce6f31a5466617794153c4731418692951a5f1ef
ianbaremans <ianbaremans@gmail.com>
2017-12-29 23:35:10 +0100

    No commit: finished session

--- main.py
+++ main.py
@@ -9,21 +9,35 @@ request = "https://api.darksky.net/forecast/{}/{},{}?units=si"
 time_machine = "https://api.darksky.net/forecast/{}/{},{},{}?units=si" 
 with open("secret_key.txt") as secret_key_file:
     secret_key = secret_key_file.read().replace("\n", "").replace("\r", "")
-unixtime = int(time.time())
 
+unixtime = int(time.time())
+# round unixtime to the next hour
+unixtime_rounded = unixtime // 3600 * 3600 + 3600
 now = datetime.datetime.now()
+now_time = int(now.strftime("%H")) + 1
 today_date = now.strftime("%A %d-%m-%Y") 
 
-response = requests.get(request.format(secret_key, latitude, longitude))
-forecast = response.json()
+print("Available commands: summary_week, rain_today.")
+command = str(input("Command: "))
 
-print("Today is "+today_date+". The forecast for today is: ")
-print(forecast["currently"]["summary"])
-print(" -"*int(len(forecast["currently"]["summary"])/2))
+if command == "summary_week":
+    response = requests.get(request.format(secret_key, latitude, longitude))
+    forecast = response.json()
 
-for num in range(unixtime+86400, unixtime+604800, 86400):
-    time_response = requests.get(time_machine.format(secret_key, latitude, longitude, num))
-    future = time_response.json()
-    day = datetime.datetime.fromtimestamp(num).strftime("%A")
-    print("{}: {}".format(day, future["currently"]["summary"]))
+    print("\nToday is "+today_date+". The forecast for today is: ")
+    print(forecast["currently"]["summary"])
+    print(" -"*int(len(forecast["currently"]["summary"])/2))
+
+    for num in range(unixtime+86400, unixtime+604800, 86400):
+        time_response = requests.get(time_machine.format(secret_key, latitude, longitude, num))
+        future = time_response.json()
+        day = datetime.datetime.fromtimestamp(num).strftime("%A")
+        print("{}: {}".format(day, future["currently"]["summary"]))
+elif command == "rain_today":
+    print("\nToday is "+today_date+". The rain-forecast for today is: ")
+    hours_left = 24 - now_time
+    print(now_time)
+    for num in range(hours_left, 24, 3600):
+        hour_response = requests.get(time_machine.format(secret_key, latitude, longitude, num))
+
 
